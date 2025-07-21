6921c1348817f5feb8a5a8476e36e411f22c49f1
gatosembucho <malu.14geraldo@gmail.com>
2025-07-21 13:56:18 -0300

    Repository created

--- EndPoints/UsuarioEndPoints.cs
+++ EndPoints/UsuarioEndPoints.cs
@@ -3,8 +3,8 @@ namespace LeleLove.EndPoints;
 public static class UsuarioEndpoints
 {
     public static void ConfigureUsuarioEndPoints() 
-     {
+    {
 
-     } 
+    } 
 
 } 
---
+++ Implementations/EFUsuarioRepositary.cs
@@ -0,0 +1,11 @@
+namespace LeleLove.Implementations;
+
+public class EFUsuarioRepository(LeleLovesDbContext ctx) : IUsuarioRepository
+{
+    public async Task<Guid?> Create(User usuario)
+    {
+        ctx.User.Add(usuario);
+        await ctx.SaveChangesAsync();
+        return usuario.Id;
+    }
+}
